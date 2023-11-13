8ed63bb51a2037ecbe207837ecfecd3d0a01e4f9
Kumori <tauen8@gmail.com>
2023-11-13 14:06:03 -0600

    it works! stored the args into an array and that into a variable and returned the length of that array and console logged the call

--- script.js
+++ script.js
@@ -3,9 +3,26 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-	
+
+const parameterArray = [...args]
+
+	return parameterArray.length
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
- */
+
+const result = argumentsLength(1, 2, 3, 4, 5, 6
+    ); // 3
+
+console.log(result)
+
+
+//I want the function to count the length of the arguments passed in
+//if the parameters were an array it would be parameterArray.length
+
+//I needed to store these args into an array in order for me to access the length of the array, so I store it in the parameterArray variable
+
+//Since the parameterArray.length is what I want in the end that is what will be my return value from the function. I could return the length or store it first and then return it
+
+//then I want to have a way of ouputting the result of the function call so I store the call in a variable and then console.log that output
+
+//Notice if I remove the return statement of the function, I get undefined
